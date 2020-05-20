b105c175f3486590eae13054669f5ee0566d53bc
Sonith LS <sonith.ls@iiap.res.in>
2020-05-20 15:56:40 +0530

    Initial Commit

---
+++ HFOSC_SPECPip.py
@@ -0,0 +1,9 @@
+# -------------------------------------------------------------------------------------------------------------------- #
+#This script is to semi-automate basic data reduction of HFOSC spectrosopic data
+#Author : Sonith L.S
+#Contact : sonith.ls@iiap.res.in
+
+#Version 0.0.10
+#Code is  written serially to check every functions are working properly
+#Adiitional formatting required for running in for multiple number of folder in faster way.
+# -------------------------------------------------------------------------------------------------------------------- #
