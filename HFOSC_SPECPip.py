87e57b44ad3b494b7206bf26163e2fb7b69bbec7
Sonith LS <sonith.ls@iiap.res.in>
2020-05-24 02:43:01 +0530

    Added comments and minor spacing correction

--- HFOSC_SPECPip.py
+++ HFOSC_SPECPip.py
@@ -14,11 +14,11 @@
 import os
 import glob
 import shutil
+
 try:
     from pyraf import iraf
 except ImportError as error:
     print (error + "Please install pyraf and iraf")
-
 
 BACKUP= "HFOSC_PIPELINE_DataBackup"
 
@@ -33,7 +33,14 @@ Backup (BACKUP)
 
 
 def search_files (location='', keyword=''):
-    """This function generate filelist from assigned folder with specific keyword in it"""
+
+    """ This function generate filelist from assigned folder with specific keyword in it.
+    Arguments:
+        location: location of the files if it is not in the working directory
+        keyword : keyword in the name of the file eg: "*.fits"
+    Returns:
+
+    """
 
     if location != '':                                                #change -- location
         pathloc = os.path.join(os.getcwd(), location)
@@ -41,17 +48,19 @@ def search_files (location='', keyword=''):
     if keyword != '':
         file_list = glob.glob1(pathloc, keyword)
 
-
     return file_list
 
 
 
 def list_subdir ():
+
     """This function list all sub directories.
-    Args: none
+    Arguments:
+        none
     Returns:
         sub_directories: name of sub-directories
     """
+
     directory_contents = os.listdir(os.getcwd())
     sub_directories = []
     for item in directory_contents:
@@ -62,20 +71,28 @@ def list_subdir ():
 
 PATH = os.path.join(os.getcwd(),list_subdir()[0])
 #print PATH
-
-
 list_files = search_files(location=list_subdir()[0], keyword='*.fits')
-print list_files
 #print list_files
 
 import shutil
+
 def spec_or_phot (file_list, location, func=''):
     """
-    Check weather the file is using for spectrosopy of photometry make sperate list of files for
-    spectrosopy and photometry
+    Check whether the file contains spectrosopy of photometry data and make sperate list
+    for both spectrosopy and photometry with respective file names.
+    Arguments:
+        file_list: List of all files in the directory from which files need to identify
+        location : location of the files if it is not in the working directory
+        func     : type of files need keep in orginal folder as it is. Remaining files
+                   will be moved to other directory.
+    Returns:
+        spec_list: List of spectrosopic files
+        phot_list: List of photometric files
     """
+
     spec_list = []
     phot_list = []
+
     for file in file_list :
         file_name = os.path.join(location,file)
         hdul = fits.open(file_name) #HDU_List
@@ -88,7 +105,6 @@ def spec_or_phot (file_list, location, func=''):
             spec_list.append(file)
         elif value <= 2 :
             phot_list.append(file)
-
 
     if func == 'spec':
         try:
@@ -112,14 +128,21 @@ def spec_or_phot (file_list, location, func=''):
 
     return spec_list, phot_list
 
+
 speclist, photlist = spec_or_phot (list_files, PATH, 'spec')
 #file_list is updated from passing list
-print (speclist)
+#print (speclist)
+
 
 def list_bias (file_list, location=''):
     """
-    Look for bias header in the files and assign them to bias files and make master
-    bias using these files and delete the processed files after the task complete
+    Identify bias files from file_list provided by looking the header keyword in the files
+    Arguments:
+        file_list   : List of all files in the directory from which files need to identify
+        location    : location of the files if it is not in the working directory
+    Returns:
+        bias_list   : List of bias files from the file_list provided
+        passing_list: Remaining files after removing bias files from the file_list
     """
 
     bias_list = []
@@ -141,8 +164,8 @@ def list_bias (file_list, location=''):
 
 
 bias_list, passing_list = list_bias (speclist, PATH)
-print (bias_list)
-print (passing_list)
+# print (bias_list)
+# print (passing_list)
 
 
 from pyraf import iraf
@@ -159,27 +182,35 @@ iraf.twodspec(_doprint=0)
 iraf.apextract(_doprint=0)
 iraf.onedspec(_doprint=0)
 iraf.ccdred.instrument = "ccddb$kpno/camera.dat"
-
+# -------------------------------------------------------------------------------------------------------------------- #
+"""CCD Information provided for running of IRAF module"""
 read_noise = 4.87
 ccd_gain = 1.22
 data_max = 55000
+# -------------------------------------------------------------------------------------------------------------------- #
 
 
 def remove_file(file_name):
-
+    """
+    Removing a file from the directory
+    """
     try:
         os.remove(file_name)
     except OSError:
         pass
 
-read_noise = 4.87
-ccd_gain = 1.22
-data_max = 55000
 
 def bias_correction (bias_list, list_file, location='', prefix_string='b_'):
     """
-    Look for bias header in the files and assign them to bias files and make master
-    bias using these files and delete the processed files after the task complete
+    From the imput bias_list make master-bias, do bias correction to rest of files in the
+    directory, remove all past files and backup master-bias file
+    Arguments:
+        bias_list    : List of bias files to make master bias. 
+        list_file    : List of files which need to do bias correction.
+        location     : location of the files if it is not in the working directory
+        prefix_string: prefix which add after doing bias correction to files.
+    Returns:
+        none
     """
 
     if location != '':                                                #change -- location
