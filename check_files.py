6ccc6e4db49b480abb44ee0e58bb5e36a3d4d4b4
Sonith LS <sonith.ls@iiap.res.in>
2020-07-07 16:09:49 +0530

    Corrected exception and time delay

--- check_files.py
+++ check_files.py
@@ -3,6 +3,7 @@ import re
 import glob
 import numpy as np
 from astropy.io import fits
+import time
 
 
 def search_files (location='', keyword=''):
@@ -19,6 +20,7 @@ def search_files (location='', keyword=''):
 
     if keyword != '':
         file_list = glob.glob1(pathloc, keyword)
+        file_list.sort()
 
     return file_list
 
@@ -29,16 +31,22 @@ count =0
 
 dash = '-' * 80
 print (dash)
-print('{:<40s}{:<10s}{:>12s}{:>12s}'.format("FILE NAME", "OBJECT", "GRISM", "APERTURE"))
+print('{:<40s}{:<20s}{:<12s}{:<12s}'.format("FILE NAME", "OBJECT", "GRISM", "APERTURE"))
 print (dash)
 
 file = open("file_info", 'w')
 for file_name in list_files :
     hdul = fits.open(file_name)
     hdr = hdul[0].header  # the primary HDU header
-    OBJECT = hdr['OBJECT']
-    GRISM = hdr['GRISM']
-    aperture = hdr['APERTUR']
+    try:
+    	OBJECT = hdr['OBJECT']
+    	GRISM = hdr['GRISM']
+    	aperture = hdr['APERTUR']
+    except :
+    	print ("No object in header of : ", file_name)
+    	OBJECT = "none"
+    	GRISM = "none"
+    	aperture = "none"
 #----------------------- VBT---------------------------#
 #     OBJECT = hdr['OBJECT']
 #     UPPER = hdr['UPPER']
@@ -46,8 +54,9 @@ for file_name in list_files :
 #     SLIT = hdr['SLIT']
 #------------------------------------------------------#
     count +=1
-    print('{:<40s}{:<10s}{:>12s}{:>12s}'.format(file_name, OBJECT, GRISM, aperture))
+    print('{:<40s}{:<20s}{:<12s}{:<12s}'.format(file_name, OBJECT, GRISM, aperture))
 #     print (file_name, OBJECT, UPPER, LOWER, SLIT)
-    file.writelines(file_name+"  "+OBJECT+"  "+GRISM+ '\n')
-
+    file.writelines('{:<40s}{:<20s}{:<12s}{:<12s}'.format(file_name, OBJECT, GRISM, aperture)+ '\n')
 print (dash)
+
+time.sleep(60)
