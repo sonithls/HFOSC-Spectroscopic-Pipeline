18c24aa2b23385497064d53f4f6673d65b2942cd
Sonith LS <sonith.ls@iiap.res.in>
2020-11-08 23:59:26 +0530

    Functions added with inquirer

--- main.py
+++ main.py
@@ -83,6 +83,34 @@ bar = """
 """
 
 
+def options(message, choices):
+    """
+    Funnction for giving multiple options while running code.
+    message : Message before giving different options.
+    choices : List of chioses
+    Ex : choices = ['Default', 'Manually']
+    """
+    question = [inquirer.List('x', message, choices)]
+    answer = inquirer.prompt(question)
+    print(answer["x"])
+    answer = answer["x"]
+    return answer
+
+
+def multioptions(message, choices, default=''):
+    """
+    Funnction for giving multiple options while running code.
+    message : Message before giving different options.
+    choices : List of chioses
+    Ex : choices = ['Default', 'Manually']
+    """
+    question = [inquirer.Checkbox('x', message, choices, default)]
+    answer = inquirer.prompt(question)
+    print(answer)
+    answer = answer["x"]
+    return answer
+
+
 def part1(flat_flag):
     # Backing up the whole directory
     # Backup (BACKUP)
