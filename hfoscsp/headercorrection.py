536a77c735b7026c947ef1830b2a76ca09c327e1
Sonith L.S <sonith.ls@iiap.res.in>
2021-07-24 16:17:39 +0530

    Header correction func for HFOSC fits files.

--- hfoscsp/headercorrection.py
+++ hfoscsp/headercorrection.py
@@ -8,6 +8,180 @@ from astroquery.simbad import Simbad
 from tabulate import tabulate
 
 
+def headcorr_k(file_list, location=''):
+    """Header correction for files in the file list."""
+    print(os.getcwd())
+    if location != '':  # change location
+        loc_object_info = os.path.join(os.getcwd(), location, 'file_info')
+        textfile = os.path.join(os.getcwd(), location, 'file_info.txt')
+    else:
+        loc_object_info = os.path.join(os.getcwd(), 'file_info')
+        textfile = os.path.join(os.getcwd(), 'file_info.txt')
+    print(loc_object_info)
+
+    data = []
+
+    file_list.sort()
+
+    for filename in file_list:
+        if location != '':  # change location
+            loc = os.path.join(os.getcwd(), location, filename)
+        else:
+            loc = os.path.join(os.getcwd(), filename)
+
+        hdu = fits.open(loc)
+        header = hdu[0].header
+
+        # HFOSC
+        if 'TM_START' in header.keys():
+            try:
+                object_name = header['OBJECT']
+            except:
+                object_name = 'NaN'
+
+            try:
+                GRISM = header['GRISM']
+            except:
+                GRISM = 'NaN'
+
+            try:
+                aperture = header['APERTUR']
+            except:
+                aperture = 'NaN'
+
+            try:
+                EXPTIME = header['EXPTIME']
+            except:
+                EXPTIME = 'NaN'
+
+        # HFOSC2
+        # Check it later
+        else:
+            try:
+                object_name = header['OBJECT']
+            except:
+                object_name = 'NaN'
+
+            try:
+                GRISM = header['GRISM']
+            except:
+                GRISM = 'NaN'
+
+            try:
+                aperture = header['APERTUR']
+            except:
+                aperture = 'NaN'
+
+            try:
+                EXPTIME = header['EXPTIME']
+            except:
+                EXPTIME = 'NaN'
+
+        data.append([filename, object_name, GRISM, aperture, EXPTIME])
+
+    # Write table in to a csv file.
+    if len(data) != 0:
+        with open(loc_object_info, 'w') as f:
+            f.write('FILENAME'+','+'OBJECT'+','+'GRISM'+',' +
+                    'APERTURE'+','+'EXPTIME'+'\n')
+            for i in range(0, len(data)):
+                f.write(data[i][0]+','+data[i][1]+','+data[i][2]+','+data[i][3]
+                        + ','+str(data[i][4])+'\n')
+                # print(data[i][1]+','+data[i][2]+','+data[i][3]+','+data[i][0]+'\n')
+        with open(textfile, 'w') as f:
+            f.write('{:<28s}{:<20s}{:<12s}{:<12s}{:<12s}'.format('FILENAME','OBJECT','GRISM', 
+                                                                 'APERTURE','EXPTIME'))
+            for i in range(0, len(data)):
+                f.write('{:<28s}{:<20s}{:<12s}{:<12s}{:<12}'.format(data[i][0], data[i][1],
+                                                                    data[i][2], data[i][3],
+                                                                    str(data[i][4])))
+
+    print("")
+    print(tabulate(data, headers=['FILENAME', 'OBJECT',
+                                  'GRISM', 'APERTURE', 'EXPTIME']))
+    print("")
+    return data
+
+
+def updateheader_k(data, location=''):
+    """Update header."""
+    for i in range(0, len(data)):
+        filename = data[i][0]
+        if location != '':  # change location
+            loc = os.path.join(os.getcwd(), location, filename)
+        else:
+            loc = os.path.join(os.getcwd(), filename)
+
+        hdu = fits.open(loc, mode='update')
+        header = hdu[0].header
+
+        object_name = data[i][1]
+        GRISM = data[i][2]
+        aperture = data[i][3]
+
+        # Check for HFOSC2
+
+        list_keywords = ['object_name', 'GRISM', 'APERTURE']
+        data_header = {'object_name': object_name, ' GRISM': GRISM,
+                       'APERTURE': aperture}
+
+        for key in list_keywords:
+            if key in header.keys():
+                header.remove(key, remove_all=True)
+            header.append(card=(key, data_header[key]))
+
+        hdu.flush()
+        hdu.close()
+
+
+def read_info_k(location=''):
+    """Read file information."""
+    if location != '':  # change location
+        loc_object_info = os.path.join(os.getcwd(), location, 'file_info')
+        textfile = os.path.join(os.getcwd(), location, 'file_info.txt')
+    else:
+        loc_object_info = os.path.join(os.getcwd(), 'file_info')
+        textfile = os.path.join(os.getcwd(), 'file_info.txt')
+    obj_info = ascii.read(loc_object_info)
+
+    # print(obj_info)
+
+    data = []
+    for i in range(0, len(obj_info)):
+        # print(i)
+        filename = obj_info['FILENAME'][i]
+        object_name = obj_info['OBJECT'][i]
+        GRISM = obj_info['GRISM'][i]
+        APERTURE = obj_info['APERTURE'][i]
+        EXPTIME = obj_info['EXPTIME'][i]
+        # print(filename)
+        data.append([filename, object_name, GRISM, APERTURE, EXPTIME])
+    # print(data)
+
+    # Write table in to a csv file.
+    if len(data) != 0:
+        with open(loc_object_info, 'w') as f:
+            f.write('FILENAME'+','+'OBJECT'+','+'GRISM'+',' +
+                    'APERTURE'+','+'EXPTIME'+'\n')
+            for i in range(0, len(data)):
+                f.write(data[i][0]+','+data[i][1]+','+data[i][2]+','+data[i][3]
+                        + ','+str(data[i][4])+'\n')
+                # print(data[i][1]+','+data[i][2]+','+data[i][3]+','+data[i][0]+'\n')
+        with open(textfile, 'w') as f:
+            f.write('{:<28s}{:<20s}{:<12s}{:<12s}{:<12s}'.format('FILENAME','OBJECT','GRISM', 
+                                                                 'APERTURE','EXPTIME'))
+            for i in range(0, len(data)):
+                f.write('{:<28s}{:<20s}{:<12s}{:<12s}{:<12}'.format(data[i][0], data[i][1],
+                                                                    data[i][2], data[i][3],
+                                                                    str(data[i][4])))
+
+    print("")
+    print(tabulate(data, headers=['FILENAME', 'OBJECT',
+                                  'GRISM', 'APERTURE', 'EXPTIME']))
+    print("")
+    return data
+
+
 def headcorr(file_list, location=''):
     """Header correction for files in the file list."""
     print (os.getcwd())
@@ -164,7 +338,7 @@ def headercorr(file_list, location=''):
     """Run the code."""
     data = headcorr(file_list, location=location)
 
-    print("Check the object_list.csv before updating the header.")
+    print("Check the 'object_info' before updating the header.")
     message = "Do you want to continue updating header ?"
     choices = ['Yes', 'No']
     answer = options(message, choices)
@@ -172,12 +346,32 @@ def headercorr(file_list, location=''):
     while answer != 'Yes':
         data = read_info(location=location)
 
-        print("object_list.csv is updated, check it before updating the header.")
+        print("'object_info' is updated, check it before updating the header.")
         message = "Do you want to continue updating header ?"
         choices = ['Yes', 'No']
         answer = options(message, choices)
 
     updateheader(data, location=location)
+
+
+def headercorr_k(file_list, location=''):
+    """Run the code."""
+    data = headcorr_k(file_list, location=location)
+
+    print("Check the 'file_info' before updating the header.")
+    message = "Do you want to continue updating header ?"
+    choices = ['Yes', 'No']
+    answer = options(message, choices)
+
+    while answer != 'Yes':
+        data = read_info_k(location=location)
+
+        print("'file_info' is updated, check it before updating the header.")
+        message = "Do you want to continue updating header ?"
+        choices = ['Yes', 'No']
+        answer = options(message, choices)
+
+    updateheader_k(data, location=location)
 
     # while True:
     # do_something()
